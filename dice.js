27f11e6277fe777e76f544122ba8ab68938fc61c
Tina <omnilemm@gmail.com>
2014-07-19 13:55:23 -0700

    Added diceFights() and countResults()

--- dice.js
+++ dice.js
@@ -1,13 +1,18 @@
-var Dice = new Object.prototype {
-	this.numFaces = 6;
-	this.faces = [1,2,3,4,5,6];
+var Dice = new Object(numFaces) {
+	this.numFaces = numFaces;
+	this.faces = new Array(numFaces);
+	for (var i = 1; i <= numFaces; i++) {
+		this.faces[i-1] = i;
+	} 
 	this.roll = function() {
 		var modifier = Math.floor((Math.random() * this.numFaces) - 1);
 		return this.faces[modifier];
 	}
 };
 
-function d6 = new Dice();
+function d6 = new Dice(6);
+function d20 = new Dice(20);
+
 
 //~ returns an array with X elements, representing the individual rolls
 //~ 	themselves for analysis.
@@ -21,15 +26,28 @@ function rollXTimes(die, x) {
 
 var roll1000 = rollXTimes(d6, 1000);
 
-function findMedian(rollsArray) {
-	var i = rollsArray[0];
-	var isSmallerThan = function(element, index, array) {
-		return (element >= i);
-	}
-	while (!rollsArray.every(isSmallerThan)) {
-			
+function countResults(rollsArray) {
+	var results = new Object();
+//~ if the current element of rollsArray is a key in results, add 1 to the value.
+//~ if not, create a new key in results and set the value to 1.
+	for (var i = 0; i < rollsArray.length; i++) {
+		if (results.hasOwnProperty(rollsArray[i]) {
+			results.rollsArray[i] += 1;
+		} else {
+			results.rollsArray[i] = 1;
+		};
+	};
+	return results;
+};
+		
+//~ returns a string of which die rolls greater, or tie.
+function dieFight(dieA, dieB) {
+	var first = dieA.roll();
+	var second = dieB.roll();
+	if (first >second) return 'dieA';
+	else if (second > first) return 'dieB';
+	else return 'tie'
+};
 
 
 
-
-	
